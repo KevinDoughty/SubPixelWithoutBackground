c6aa4521a5d7ab43d8d4ee058057d3abe2281306
Kevin Doughty <github@kxdx.org>
2014-03-12 21:05:10 -0400

    new project

---
+++ SubPixelWithoutBackground/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SubPixelWithoutBackground
+//
+//  Created by Kevin Doughty on 3/12/14.
+//  Copyright (c) 2014 Kevin Doughty. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
---
+++ SubPixelWithoutBackgroundTests/SubPixelWithoutBackgroundTests.m
@@ -0,0 +1,34 @@
+//
+//  SubPixelWithoutBackgroundTests.m
+//  SubPixelWithoutBackgroundTests
+//
+//  Created by Kevin Doughty on 3/12/14.
+//  Copyright (c) 2014 Kevin Doughty. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SubPixelWithoutBackgroundTests : XCTestCase
+
+@end
+
+@implementation SubPixelWithoutBackgroundTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
